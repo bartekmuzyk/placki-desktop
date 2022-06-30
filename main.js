7c23404d194ef34fea5877577763e4304c6f817f
bartekmuzyk <imejl.bartek@gmail.com>
2022-07-01 00:35:22 +0200

    sesja jest teraz zapamiętywana przez przeglądarkę bez pomocy aplikacji pulpitowej

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-const { app, BrowserWindow, dialog } = require("electron");
-const Store = require("electron-store")
+const { app, BrowserWindow } = require("electron");
 const fetch = require("node-fetch");
 
-const persistence = new Store();
 const serviceInfoUrl = "http://plackistatus.000webhostapp.com";
-let sessionFetched = false;
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -31,64 +28,7 @@ const createWindow = () => {
             if (maintenance) {
                 win.loadFile("maintenance.html");
             } else {
-                const setupNavigateHandler = () => win.webContents.on("did-navigate", async () => {
-                    if (sessionFetched || !win.webContents.getURL().endsWith("/glowna")) return;
-
-                    const matches = await win.webContents.session.cookies.get({ name: "PHPSESSID" });
-                    if (matches.length === 0) return;
-
-                    persistence.set("session_id", matches[0].value);
-                    console.log("zapisywanie id sesji: " + persistence.get("session_id"));
-                    sessionFetched = true;
-                });
-
-                win.loadURL(url)
-                    .then(async () => {
-                        /** @type {?string} */
-                        const savedSessionId = persistence.get("session_id");
-
-                        if (typeof savedSessionId === "string") {
-                            const domain = (new URL(url)).hostname;
-                            const session = win.webContents.session;
-
-                            await session.clearStorageData({ storages: ["cookies"] });
-                            await session.cookies.set({
-                                url,
-                                name: "abuse_interstitial",
-                                value: domain,
-                                domain,
-                                path: "/",
-                                secure: true,
-                                httpOnly: false,
-                                expirationDate: Date.now() / 1000 + 153600000,  // 100 years
-                                sameSite: "unspecified"
-                            });
-
-                            try {
-                                await session.cookies.set({
-                                    url,
-                                    name: "PHPSESSID",
-                                    value: savedSessionId,
-                                    domain,
-                                    path: "/",
-                                    secure: false,
-                                    httpOnly: false,
-                                    sameSite: "unspecified"
-                                });
-                            } catch (e) {
-                                persistence.delete("session_id");
-                                dialog.showErrorBox(
-                                    "nie udało się zalogować automatycznie",
-                                    "nie udało się wczytać danych o sesji. zaloguj się ponownie."
-                                );
-                                setupNavigateHandler();
-                            }
-
-                            win.webContents.reload();
-                        } else {
-                            setupNavigateHandler();
-                        }
-                    });
+                win.loadURL(url);
             }
         })
         .catch(() => {
